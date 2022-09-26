398254c136e3f8ada473fad405878eaa0034e5f0
Nadson <nadsonsilva05@gmail.com>
2022-09-26 17:09:54 -0300

    Ultimos ajustes

--- api.py
+++ api.py
@@ -1,12 +1,9 @@
 import os
 from re import template
-from urllib import request
 import face_recognition
 import cv2
-import json
 import pymongo
 from fastapi import FastAPI, UploadFile, File, Form, Request, Body
-from pydantic import BaseModel
 import shutil
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -16,33 +13,42 @@ templates = Jinja2Templates(directory='template')
 
 app = FastAPI()
 
+# Rota que contém o formulario
+
 
 @app.get('/', response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-
-# @app.post('/test2', response_class=HTMLResponse)
-# async def test(name: str = Form(...), ra: str = Form(...), email: str = Form(...), file: UploadFile = File(...)):
-#     return '<h3>Enviado com Sucesso!</h3>'
+# Rota para envio dos dados para o banco
 
 
-@app.post('/test')
-async def test(name: str = Form(...), ra: str = Form(...), email: str = Form(...), file: UploadFile = File(...)):
-    info = {
-        '_id': ra,
-        'name': name,
-        'email': email
-    }
+@app.post('/upload')
+async def upload(name: str = Form(...), ra: str = Form(...), email: str = Form(...), file: UploadFile = File(...)):
+    # Recebedo dados do formulario
+    try:
+        info = {
+            '_id': ra,
+            'name': name,
+            'email': email
+        }
+    except Exception as e:
+        return {
+            'msg': e
+        }
 
+    # Recebendo a imagem
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    # Conexão com o banco
     mongo = pymongo.MongoClient("localhost")
     db = mongo["tcc"]
-    collection = db["test"]
+    collection = db["data"]
 
+    # Ler a imagem
     image = cv2.imread(file.filename)
+    # Criação dos encodings
     try:
         info["face_encoding"] = face_recognition.face_encodings(image)[
             0].tolist()
@@ -52,16 +58,19 @@ async def test(name: str = Form(...), ra: str = Form(...), email: str = Form(...
             'msg': e
         }
 
+    # Remoção da imagem utilizada
     try:
         os.remove(file.filename)
     except:
         print(f"Falha ao remover o arquivo {file.filename}")
 
+    # Enviando dados para o banco
     try:
         collection.insert_one(info)
     except Exception as e:
         return {"Error": e}
 
+    # Encerrando conecão com o banco
     mongo.close()
 
     return {
--- face_recognition_code.py
+++ face_recognition_code.py
@@ -1,22 +1,26 @@
 import cv2
 import numpy as np
 import face_recognition
-import json
 import pymongo
 
-# Databse connection
+# Conexão ao banco de dados
 mongo = pymongo.MongoClient("localhost")
 db = mongo["tcc"]
-collection = db["test"]
+collection = db["data"]
 data = collection.find({})
 
 known_faces_encodings = []
 known_faces_ra = []
 
+# Ler cada dado recebido do banco
 for document in data:
+    # Adicionar nome e id na lista
     first_name = str(document['name']).split(' ')[0]
     known_faces_ra.append(f"{first_name} : {document['_id']}")
+    # Adicionar o encoding do rosto na lista
     known_faces_encodings.append(np.array(document["face_encoding"]))
+
+# Encerrando conexão com o banco
 mongo.close()
 
 # Iniciar camera
@@ -29,48 +33,43 @@ while True:
     frame = cv2.flip(frame, 1)
     if ret:
         try:
-            # cv2.imshow('frame', frame)
+            # Descobrir as localizações dos rostos no frame
             face_locations = face_recognition.face_locations(frame)
-            print(face_locations)
             frame_encoding = face_recognition.face_encodings(
                 frame, face_locations)
 
-            found = []
-
-            # Loop through faces in frame
+            # Identificar todos os rostos no frame
             for (top, right, bottom, left), face_encoding in zip(face_locations, frame_encoding):
+                # Comparar rosto no frame com os encodings do banco
                 matches = face_recognition.compare_faces(
                     known_faces_encodings, face_encoding, tolerance=0.55)
 
-                # Defaut face name and color
+                # Nome e cor padrão
                 name = "Unkown"
                 rect_color = (5, 5, 215)
 
-                # If match found
+                # Caso o rosto seja reconhecido
                 if True in matches:
                     first_match_index = matches.index(True)
                     name = known_faces_ra[first_match_index]
-                    found.append(name)
                     rect_color = (5, 200, 5)
 
-                # Draw rectangle arround the face
+                # Desenhar um retângulo ao redor do rosto
                 frame = cv2.rectangle(frame, (left, top),
                                       (right, bottom - 15), rect_color)
-                # Draw rectangle to put the name
+                # Desenhar um retângulo para colocar o nome
                 frame = cv2.rectangle(
                     frame, (left, bottom), (right, bottom - 15), rect_color, -1)
-                # Draw the name and id
+                # Colocar o nome e id
                 frame = cv2.putText(frame, name, (left + 2, bottom - 3),
                                     cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255, 255, 255, 255))
 
-            print(found)
-
-            # Display Image
+            # Mostrar o frame
             cv2.imshow('Video', frame)
             if cv2.waitKey(5) == 27:
                 break
 
-        # Show error if it ever happens
+        # Mostrar erro caso aconteça
         except Exception:
             print(Exception)
 
